7035eed1e04355fc1c46963e1e594edfb534aaae
memwangi <donmwas007@gmail.com>
2019-01-31 18:19:59 +0300

    Created Views

--- polls/urls.py
+++ polls/urls.py
@@ -3,5 +3,14 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    # Example: /polls/
+    path('', views.index, name='index'),
+    # Example: /polls/5
+    path('<int:question_id>/', views.detail, name='detail'),
+    # Example: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # Example: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+    
+
 ]
