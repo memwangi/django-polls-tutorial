8bb8727dc0e0684b5951be3f94bc971f521331e5
memwangi <donmwas007@gmail.com>
2019-02-04 00:46:50 +0300

    Created Tests

--- polls/urls.py
+++ polls/urls.py
@@ -2,15 +2,16 @@ from django.urls import path
 from . import views
 
 
+app_name = 'polls'
 urlpatterns = [
     # Example: /polls/
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     # Example: /polls/5
-    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     # Example: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     # Example: /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
-    
+
 
 ]
